ca62824203acc568a4199fae509ded5575eeb03d
Gonzalo Ramirez <gonzalogabrielramirez@outlook.com.ar>
2025-12-14 22:19:13 -0300

    Sección Nuestra misión finalizada. Se agregaron animaciones varias

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from './components/Header';
 import MobileMenu from './components/MobileMenu';
 import Target from './components/Target';
@@ -9,6 +9,33 @@ function App() {
 
     // Función para alternar el estado del menú
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    function useIsVisible() {
+        const [isVisible, setIsVisible] = useState(false);
+        const ref = useRef(null);
+
+        useEffect(() => {
+            const observer = new IntersectionObserver(([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                    observer.disconnect();
+                }
+            }, {
+                threshold: 0.5,
+                rootMargin: "0px 0px -100px 0px"
+            });
+
+            if (ref.current) {
+                observer.observe(ref.current);
+            }
+
+            return () => observer.disconnect()
+        }, []);
+
+        return { ref, isVisible }
+    }
+
+    const { ref: titleRef, isVisible } = useIsVisible()
 
     return (
         <div className="min-h-screen flex flex-col">
@@ -24,10 +51,10 @@ function App() {
 
                     {/* Texto principal (z-20 para estar por encima de todo) */}
                     <div className="relative z-20 flex flex-col items-center justify-center min-h-[calc(100vh-64px)] w-full p-6 text-center">
-                        <h2 className="text-5xl font-extrabold mb-4 leading-snug text-stone-700">
+                        <h2 className="text-5xl font-extrabold mb-4 leading-snug text-stone-700 animate-slide-right">
                             Descubrí la verdad sobre cada clase.
                         </h2>
-                        <p className="text-lg mb-8 max-w-sm text-stone-500">
+                        <p className="text-lg mb-8 max-w-sm text-stone-500 animate-delay-500 animate-slide-left">
                             La plataforma anónima donde los estudiantes evalúan a sus profesores, tal como ellos a nosotros.
                         </p>
                         <a 
@@ -40,7 +67,7 @@ function App() {
                     </div>
 
                     {/* Tarjeta dispersa 1 */}
-                    <div className="absolute top-[10%] left-[-20%] transform -rotate-350 z-0">
+                    <div className="absolute top-[10%] left-[-60%] opacity-0 transform -rotate-350 z-0 animate-fade-target-1">
                         <Target
                             teacherName={"Prof. Julieta Prandi"}
                             university={"UNAB"}
@@ -52,7 +79,7 @@ function App() {
                     </div>
 
                     {/* Tarjeta dispersa 2 */}
-                    <div className="absolute top-[10%] left-[70%] transform -rotate-20 z-0">
+                    <div className="absolute top-[10%] left-[125%] opacity-0 transform -rotate-20 z-0 animate-fade-target-2">
                         <Target
                             teacherName={"Prof. Mariana Bayeslian"}
                             university={"UNAB"}
@@ -64,7 +91,7 @@ function App() {
                     </div>
 
                     {/* Tarjeta dispersa 3 */}
-                    <div className="absolute top-[60%] left-[-30%] transform -rotate-350 z-0">
+                    <div className="absolute top-[60%] left-[-60%] opacity-0 transform -rotate-350 z-0 animate-fade-target-3 animate-delay-300">
                         <Target
                             teacherName={"Prof. Florencia Statti"}
                             university={"UNAB"}
@@ -76,7 +103,7 @@ function App() {
                     </div>
 
                     {/* Tarjeta dispersa 4 */}
-                    <div className="absolute top-[60%] right-[-40%] transform -rotate-30 z-0">
+                    <div className="absolute top-[70%] right-[-40%] opacity-0 transform -rotate-30 z-0 animate-fade-target-4">
                         <Target
                             teacherName={"Prof. Margaret Colapinto"}
                             university={"UNAB"}
@@ -92,9 +119,8 @@ function App() {
             <section className="w-full max-w-7xl mx-auto py-16 bg-white">
                 <div className="max-w-lg mx-auto p-4 text-center flex flex-col items-center">
 
-                    <h2 className="text-4xl font-extrabold mb-4 text-stone-700">
-                        Nuestra misión
-                    </h2>
+                    <h2 ref={titleRef} className={`text-4xl font-extrabold mb-4 text-stone-700 ${
+                    isVisible ? 'animate-typewriter' : 'opacity-0'}`}>Nuestra misión</h2>
 
                     <PillBadge
                         text="Equilibrando poderes"
